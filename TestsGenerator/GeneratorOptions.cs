23a3d74cefdb6f6fb3b6f194118c84f7b1f52840
Max <maks_kosten@mail.ru>
2022-11-20 16:55:51 +0300

    Generator options

---
+++ TestsGenerator/GeneratorOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    class GeneratorOptions
+    {
+        public int MaxRead { get; set; } = 5;
+
+        public int MaxGenerate { get; set; } = 5;
+
+        public int MaxWrite { get; set; } = 5;
+
+        public string SourceDirectory { get; set; }
+
+        public string DestinationDirectory { get; set; }
+    }
+}
