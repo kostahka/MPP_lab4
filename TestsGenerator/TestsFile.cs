2b92cff99c211259509fa8c54c5c2b74c525f6e9
Max <maks_kosten@mail.ru>
2022-11-20 18:28:37 +0300

    Basic representation of blocks in dataflow

---
+++ TestsGenerator/TestsFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    class TestsFile
+    {
+        public string Filename { get; }
+
+        public string Content { get; }
+
+        public TestsFile(string filename, string content)
+        {
+            Filename = filename;
+            Content = content;
+        }
+    }
+}
--- TestsGenerator/TestsGenerator.cs
+++ TestsGenerator/TestsGenerator.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.IO;
 
 namespace TestsGenerator
 {
-    public class TestsGenerator
+    static public class TestsGenerator
     {
+        private static TransformManyBlock<string, string> CreateReadDirectoryBlock() =>
+            new TransformManyBlock<string, string>(path =>
+                {
+                    if (!Directory.Exists(path))
+                    {
+                        throw new ArgumentException("Directory doesn't exists");
+                    }
+
+                    return Directory.EnumerateFiles(path);
+                }
+            );
+
+        private static TransformBlock<string, string> CreateReadFileBlock(int maxParallelism) =>
+            new TransformBlock<string, string>( async path =>
+                    {
+                        if (!File.Exists(path))
+                        {
+                            throw new ArgumentException("File doesn't exist");
+                        }
+
+                        var file = File.OpenText(path);
+                        return await file.ReadToEndAsync();
+                    }
+                , new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = maxParallelism }
+                );
+
+        /*private static TransformManyBlock<string, string> CreateSplitClassesBlock() =>
+            new TransformManyBlock<string, string>( code => 
+            {
+                return default;    
+            } );*/
+
+        private static TransformBlock<string, string> CreateTestGeneratorBlock(int maxParallelism) =>
+            new TransformBlock<string, string>( GenerateTests,new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = maxParallelism });
+
+        private static ActionBlock<TestsFile> CreateWriterBlock(string path, int maxParallelism) =>
+            new ActionBlock<TestsFile>(testFile =>
+           {
+               if (!Directory.Exists(path))
+               {
+                   throw new ArgumentException("Directory doesn't exists");
+               }
+
+               var file = File.CreateText($"{path}/{testFile.Filename}");
+               return file.WriteAsync(testFile.Content);
+           }, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = maxParallelism });
+
+        private static string GenerateTests(string classCode)
+        {
+            return default;
+        }
     }
 }
